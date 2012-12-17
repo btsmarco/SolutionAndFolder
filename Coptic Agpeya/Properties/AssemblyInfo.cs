a7c5cbf902f0e37ee11ce8f2fe0204607b12aa4f
beshoy hanna <bigshot929@yahoo.com>
2012-12-17 12:32:42 -0500

    Beshoy Hanna
    * Tested app on Dual-Core and WP8 phones. no problems
    * Edited home page and about.
    * Ready For Deployment to Windows Store.
    
    TO-DO
    
    jumplist to specific agypeya prayers
    scrolling homescreen

--- Coptic Agpeya/Properties/AssemblyInfo.cs
+++ Coptic Agpeya/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Resources;
 [assembly: AssemblyTitle("Coptic_Agpeya")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Arab Money")]
 [assembly: AssemblyProduct("Coptic_Agpeya")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
